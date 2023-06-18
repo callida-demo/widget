19e91ba341152764452710ef69c242c674e802cd
Flannery <rose.flannery@sap.com>
2023-06-19 09:25:14 +1000

    Update js and json.

--- download_csv.js
+++ download_csv.js
@@ -1,14 +1,13 @@
 (function() {
 	let template = document.createElement("template");
 	template.innerHTML = `<style></style>`;
-	
-	class CSVDownload extends HTMLElement {
+
+	customElements.define("goverp-sac-csvdownload", class CSVDownload extends HTMLElement {
 		constructor() {
 			super();
 			this._shadowRoot = this.attachShadow({mode: "open"});
 			this._shadowRoot.appendChild(template.content.cloneNode(true));
 		}
 		
-	}	
-	customElements.define("goverp-sac-csvdownload", CSVDownload);
-}) ();
+	});	
+})();
