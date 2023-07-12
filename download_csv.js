a385fdfae3b5dcb49ffaa96f8bcdd24350a88779
Flannery <rose.flannery@sap.com>
2023-07-13 09:42:40 +1000

    Some changes to make monthly actuals work.

--- download_csv.js
+++ download_csv.js
@@ -105,8 +105,11 @@
 	function parseRow(row, acc_member, _description, _comment, table_type) {
 		let rowString = ""
 		switch (table_type) {
-			case "Monthly":
+			case "Monthly Profiles":
 				rowString = parseMonProRow(row, acc_member, _description, _comment);
+				break;
+			case "Monthly Actuals":
+				rowString = parseMonActRow(row, acc_member, _description, _comment);
 				break;
 			case "Annual Actuals":
 				rowString = parseAnnActRow(row, acc_member, _description, _comment);
@@ -297,6 +300,56 @@
 		return rowString;
 	}
 	
+	function parseMonActRow(row, acc_member, _description, _comment) {
+		let _program = row["GOVERP_PROGRAM"].id;
+		let _account = row["GOVERP_CBMSACCOUNT"].id.split('&')[1].replace('[', '').replace(']', '');
+		let _related_agency = row["GOVERP_RELATEDAGENCY"].id;
+		let _spp = row["GOVERP_SPP"].id.replace('-', '');
+		let _appropriation = row["GOVERP_APPROPRIATION"].id;
+		let _jurisdiction = row["GOVERP_JURISDICTION"].id;
+		let _movement_account = row["GOVERP_MOVEMENTACCOUNT"].id;
+		let _reasonCode = "1038";
+		
+		let indicator = acc_member.properties["GOVERP_CBMSACCOUNT.INDICATOR"];
+		let mv_indicator = row["GOVERP_MOVEMENTACCOUNT"].properties["GOVERP_MOVEMENTACCOUNT.INDICATOR"];
+		
+		if (mv_indicator !== "") {
+			indicator = mv_indicator;
+		}
+		
+		let _amount = row["GOVERP_CBMSACCOUNT"].formattedValue.replace(',', '');
+		if (indicator === "CR") {
+			_amount = -_amount;
+		}
+						
+		let _month = row["Date"].description;
+		if (_month === '#') {
+			_month = '';
+		}
+		_month = _month.replace('P', '0'); //e.g 'P05' becomes '005'
+		
+		
+		// Row elements must be in same order as header string!
+		let rowElements = [
+		_month, 
+		_program, 
+		_account, 
+		_related_agency, 
+		_spp, 
+		_appropriation, 
+		_jurisdiction, 
+		_movement_account,
+		_description, 
+		_comment, 
+		_amount, 
+		_reasonCode
+		];
+		
+		rowString = joinRowElements(rowElements);
+		
+		return rowString;
+	}
+	
 	function joinRowElements(row_elements) {
 		// Replace null values with blanks as per CBMS format requirements.
 		for (var i = 0; i < 9; i++) {
@@ -314,11 +367,14 @@
 			case "Annual Estimates": 
 				_stringArray = ["Program, Reason Code, Account, Related Agency, SPP, Appropriation, Jurisdiction, Movement Account, Measure, Adjustment Description, Cmt_Justification, RB Amount, NB Amount, FE 1, FE 2, FE 3, FE 4, FE 5, FE 6, FE 7, FE 8, FE 9, FE 10, FE 11, FE 12, FE 13"];				
 				break;
-			case "Monthly":						
+			case "Monthly Profiles":						
 				_stringArray = ["Month, Program, Account, Related Agency, SPP, Appropriation, Jurisdiction, Movement Account, Adjustment Description, Cmt_Justification, YTD Amount, Reason Code"];
 				break;
 			case "Annual Actuals":
 				_stringArray = ["Program, Account, Journal Title, Related Agency, SPP, Appropriation, Jurisdiction, Movement Account, Cmt_Justification, Amount, Reason Code"];
+				break;
+			case "Monthly Actuals":						
+				_stringArray = ["Month, Program, Account, Related Agency, SPP, Appropriation, Jurisdiction, Movement Account, Reason Code, Journal Title, Cmt_Justification, YTD Amount"];
 				break;
 		}
 		return _stringArray;
